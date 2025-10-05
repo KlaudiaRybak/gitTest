091ad0a62980e6953e8924fb1b23ead5e82607ac
Klaudia Rybak <klaudia.rybak@opi.org.pl>
2025-10-05 17:53:51 +0200

    Third Class edited from branch 2

--- src/main/java/org/example/ThirdClass.java
+++ src/main/java/org/example/ThirdClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class ThirdClass {
     public static void main(String[] args) {
         System.out.println("Hello from my branch");
+        System.out.println("Hello from my branch 2");
     }
 }
