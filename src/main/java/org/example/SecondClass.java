a80cb4bdf813e14661ae98fabe4f3e7bf27a5df9
Klaudia Rybak <83862218+KlaudiaRybak@users.noreply.github.com>
2025-10-05 15:34:18 +0200

    SecondClass updated from GitHub 2

--- src/main/java/org/example/SecondClass.java
+++ src/main/java/org/example/SecondClass.java
@@ -5,6 +5,7 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub");
         System.out.println("Hello Poland");
+        System.out.println("Hello from GitHub AGAIN");
     }
 
 
