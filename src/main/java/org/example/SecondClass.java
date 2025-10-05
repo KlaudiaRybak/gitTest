a9d000ad5b8c8ba6c3f1bdd95b076b815aa65067
Klaudia Rybak <klaudia.rybak@opi.org.pl>
2025-10-05 15:09:24 +0200

    SecondClass modified 1

--- src/main/java/org/example/SecondClass.java
+++ src/main/java/org/example/SecondClass.java
@@ -5,4 +5,7 @@ public class SecondClass {
       public static void main(String[] args) {
         System.out.println("Hello from GitHub");
     }
+    public static void main(String[] args) {
+        System.out.println("Hello Poland");
+    }
 }
